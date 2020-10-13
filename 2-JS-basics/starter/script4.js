e63a17eeb9148e93ae3a99226e715d3ffd93298d
Scaryfencer <garciaarr@gmail.com>
2020-10-13 14:49:40 -0700

    finished coding challenge 5

--- 2-JS-basics/starter/script4.js
+++ 2-JS-basics/starter/script4.js
@@ -1,15 +1,3 @@
-// var john = ['John', 'Smith', 1990, 'designer', false];
-// for (var i = 0, i < john.length, i++)
-
-// var john = {
-//     // The first item is the key, and the second is the value. This is key/value pair.
-//     firstName: 'John',
-//     lastName: 'Smith',
-//     birthYear: 1990,
-//     family: ['Jane', 'Mark', 'Bob', 'Emily'],
-//     job: 'teacher',
-//     isMarried: false,
-// };
 
 // John 124 48 268 180 42 20%<50 15% 50-200  10%>200
 // objects and loops
@@ -19,7 +7,6 @@
 // as an output create:
 // 1. new array containing all the tips
 // 2. array containing final paid amounts (bill + tip) HINT start with 2 empty arrays and fill them in the loop
-
 
 var john = {
 
@@ -31,8 +18,11 @@ var john = {
     tipPercent3: 10,
     tips: [],
     billsAndTips: [],
+    tipsAvg: 0,
 
 };
+
+// EXTRA Mark family bills 77 375 110 45 mark tips  20%<100 10% 100-300  25%>300
 
 var mark = {
 
@@ -44,10 +34,63 @@ var mark = {
     tipPercent3: 25,
     tips: [],
     billsAndTips: [],
+    tipsAvg: 0,
 
 };
+   
+// Pass an object that has the bills in, loop through the bills value
+// add the tip amounts to the tip key value in the object
+// add the total of tips and bills to the tipsandbills key value in the object
+// Use the rules in the object to figure tips
+
+function calcTips(billsObject) {
+
+    for (let i = 0; i < billsObject.bills.length; i++) {
+        
+        if (billsObject.bills[i] < billsObject.limit1) {
+            billsObject.tips.push(billsObject.bills[i] * billsObject.tipPercent1 / 100);
+            billsObject.billsAndTips.push(billsObject.bills[i] + (billsObject.bills[i] * billsObject.tipPercent1 / 100));
+        } else if (billsObject.bills[i] >= billsObject.limit1 && billsObject.bills[i] <= billsObject.limit2) {
+            billsObject.tips.push(billsObject.bills[i] * billsObject.tipPercent2 / 100);
+            billsObject.billsAndTips.push(billsObject.bills[i] + (billsObject.bills[i] * billsObject.tipPercent2 / 100));
+        } else {
+            billsObject.tips.push(billsObject.bills[i] * billsObject.tipPercent3 / 100);
+            billsObject.billsAndTips.push(billsObject.bills[i] + (billsObject.bills[i] * billsObject.tipPercent3 / 100));
+        }
+    }
+
+}
+
+calcTips(john);
+calcTips(mark);
+
+// create a function not method to calc the average of a given array of tips
+// HINT: Loop over the array and in each iteration store the current sum in a var starting 0
+// after sum of array, divide by number of elements in array
+
+function calcTipsAvg(tipsObject) {
+
+    var tipsTotal = 0;
+    for (let i = 0; i < tipsObject.tips.length; i++) {
+        // for each loop, add this value to avg
+        tipsTotal += tipsObject.tips[i]; 
     
-for (let i = 0; i < billsObject.bills.length; i++) {
-    console.log(billsObject.bills[i]);
+    }
     
+    tipsObject.tipsAvg = (tipsTotal / tipsObject.tips.length);
+}
+
+calcTipsAvg(john);
+calcTipsAvg(mark);
+
+console.log(john.tips, john.billsAndTips, john.tipsAvg)
+console.log(mark.tips,mark.billsAndTips, mark.tipsAvg)
+
+// Calc the avg tip for each family
+// log to console which family paid the highest tips ON AVERAGE
+
+if (john.tipsAvg > mark.tipsAvg) {
+    console.log("John is the better average tipper.")
+} else {
+    console.log("Mark is the better average tipper.")
 }
--- 2-JS-basics/starter/temp.js
+++ 2-JS-basics/starter/temp.js
@@ -4,28 +4,10 @@
 // add the total of tips and bills to the tipsandbills key value in the object
 // Use the rules in the object to figure tips
 
-function tipCalculator(billsObject) {
-
-    //Loop through bills and do the following on each bill
+//Loop through bills and do the following on each bill
 
     
 
-    if (billsObject.limit1 < billsObject.limit1) {
-        //billsAndTips.push(billAmt * 1.2);
-    } else if (billAmt >= 50 && billAmt <= 200) {
-        billsAndTips.push(billAmt * 1.15);
-    } else {
-        billsAndTips.push(billAmt * 1.1);
-    }
-
-}
-
-// Turn this into a loop instead
-tipCalculator(bills[0]);
-tipCalculator(bills[1]);
-tipCalculator(bills[2]);
-
-console.log(bills,billsAndTips);
 
 // EXTRA Mark family bills 77 375 110 45 mark tips  20%<100 10% 100-300  25%>300
 // create a function not method to calc the average of a given array of tips
